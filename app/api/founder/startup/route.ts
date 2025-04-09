1c04f905b9e66d8fa31d7aa8a3e578686f2d274c
aayushk9 <ayushkokate1@gmail.com>
2025-04-09 19:07:40 +0530

    feat(startup): slug generation done

--- app/api/founder/startup/route.ts
+++ app/api/founder/startup/route.ts
@@ -1,14 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod'
 
-export function POST(req:NextRequest){
-    // accept the startup name from frontend and acccording to startup name postfix it and generate link
-    //  as localhost:3000/startupname (zerobatch.com/startupname)
-  return NextResponse.json({
-    msg: "Testing post request"
-  })
+const startupNameSchema = z.string()
+
+function generateSlug(startupname: String) {
+  return startupname
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
 }
 
-export function GET(req:NextRequest) {
+// generate unique slug to ensure startup name duplicated are not created
+function uniqueSlug(baseSlug: String) {
+  let slug = baseSlug
+
+  // find this slug in db if found increment by 1 and save it in db
+  // else return slug
+}
+
+export async function POST(req: NextRequest) {
+
+  try {
+    const body = await req.json(); 
+    const startupName = startupNameSchema.parse(body.startupName)
+
+    const tempSlug = generateSlug(startupName);
+    const slug = await uniqueSlug(tempSlug)
+
+    return NextResponse.json({
+      slugURL: `https://zerobatch.com/${tempSlug}`
+    }, {
+      status: 200
+    }
+    )
+  } catch (e) {   
+    console.log(e)
+    return NextResponse.json({
+      message: "please enter valid startup name"
+    }, {
+      status: 411
+    })
+  }
+}
+
+export function GET(req: NextRequest) {
   // render nothing as of now (planned)
   return NextResponse.json({
     msg: "Testing get request at startup route"
