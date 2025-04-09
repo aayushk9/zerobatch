7a441e3883ad698e29085c7da1fac8631abf0d9c
aayushk9 <ayushkokate1@gmail.com>
2025-04-09 12:43:46 +0530

    new migration

---
+++ prisma/migrations/20250409065839_update_user/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `provider` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('Google');
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "name",
+ADD COLUMN     "provider" "Provider" NOT NULL;
