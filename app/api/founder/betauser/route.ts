453349b6cb1df5b8ea45850fdf21a09d93b1e17b
aayushk9 <ayushkokate1@gmail.com>
2025-04-09 12:57:39 +0530

    auth functionality done with google

--- app/api/founder/betauser/route.ts
+++ app/api/founder/betauser/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from '@/auth'
+import { prisma } from '@/lib/prisma' 
 
 export function GET(req: NextRequest) {
+   const auth = await nexr
    return NextResponse.json({
     msg: "Testing get route at betauser route"
    })
