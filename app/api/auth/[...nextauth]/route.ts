133f754a3c4dd255e02cd6448f9c526aeda0b1a1
aayushk9 <ayushkokate1@gmail.com>
2025-04-10 23:00:21 +0530

    authentication with google achieved

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,50 @@
-import GoogleProvider from 'next-auth/providers/google'
-import NextAuth from 'next-auth'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+import { prismaClient } from "@/lib/db";
 
-const handler = NextAuth({
-    providers: [
-       GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-       })   
-    ]
-})
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.email) return false;
 
-export {handler as GET, handler as POST}   
+      // Store user email if not already present
+      const existingUser = await prismaClient.user.findUnique({
+        where: { email: user.email },
+      });
+
+      if (!existingUser) {
+        await prismaClient.user.create({
+          data: { 
+            email: user.email,
+            provider: "Google"
+        },
+        });
+      }
+
+      return true;
+    },
+    async session({ session }) {
+      // Optional: you can attach the DB user ID here
+      const dbUser = await prismaClient.user.findUnique({
+        where: { email: session.user?.email},
+      });
+
+      if (dbUser && session.user) {
+        session.user.id = dbUser.id;
+      }
+
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST };
