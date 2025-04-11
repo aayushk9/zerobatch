be892c5c15e14f934fc356c97b3bf3914b0e04b2
aayushk9 <ayushkokate1@gmail.com>
2025-04-11 23:39:00 +0530

    home page auth done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
-import { HomePage } from "@/components/HomePage";
+import { LandingPage } from '@/components/landing-page'
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if(session) {
+    redirect("/dashboard")
+  }
   return (
     <div>
-      <HomePage/>
+      <LandingPage/> 
     </div>
   );  
 }
