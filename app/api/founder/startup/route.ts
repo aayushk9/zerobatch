3cbd62065f9be4a099b057ed1454265bc2a8cf7a
aayushk9 <ayushkokate1@gmail.com>
2025-04-11 23:41:31 +0530

    feat: replace zerobatch.com with localhost:3000

--- app/api/founder/startup/route.ts
+++ app/api/founder/startup/route.ts
@@ -65,7 +65,7 @@ export async function POST(req: NextRequest) {
     })
     console.log(slug)
     return NextResponse.json({ 
-      slugURL: `https://zerobatch.com/${slug}/${joinCode}`
+      slugURL: `http://localhost:3000/${slug}/${joinCode}` 
     }, {
       status: 200
     }
